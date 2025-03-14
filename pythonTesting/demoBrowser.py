f369a34353da92d3e6baba2117cf6d2ac9402db4
Ducker <mateuszczerewko@gmail.com>
2025-03-14 16:12:10 +0100

    feat: adding first selenium tests

---
+++ pythonTesting/demoBrowser.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+#when VPN, locally chrome or some other issues ?
+# service_obj = Service("../chromedriver")
+# driver = webdriver.Chrome(service=service_obj)
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+driver.maximize_window()
+print(driver.title)
+
+
+
+
+
+
+
+
+
+time.sleep(1)
