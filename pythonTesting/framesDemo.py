f59ff1bb20fe1fe047edf53444c49794fa72bfd9
Ducker <mateuszczerewko@gmail.com>
2025-03-31 22:22:53 +0200

    feat: handling frames

---
+++ pythonTesting/framesDemo.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+#config
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(2)
+
+driver.get("https://the-internet.herokuapp.com/iframe")
+driver.switch_to.frame('mce_0_ifr')
+driver.find_element(By.ID, 'tinymce').clear()
+driver.find_element(By.ID, 'tinymce').send_keys('I am able to automate frames')
+driver.switch_to.default_content()
